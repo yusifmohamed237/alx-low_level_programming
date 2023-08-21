7a8a8b62f7de9ad8d112f985b3f1ff2bf4e4a8e6
Youssef <yusifmohamed2023@gmail.com>
2023-08-21 10:32:03 -0700

    hi

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - function that prints half of a string, followed by a new line.
+ *
+ * @n: element parameter
+ * @a: string parameter
+ *
+ * Return: nothing
+*/
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+if (i != (n - 1))
+printf("%d, ", n[i]);
+else
+printf("%d", a[i]);
+}
+}
+
