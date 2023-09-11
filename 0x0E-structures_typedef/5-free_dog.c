6c5b8426f49b931f6d6b2174a02248c9c82b0a89
Youssef <yusifmohamed2023@gmail.com>
2023-09-11 10:40:58 -0700

    hi

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+return (dest);
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - ...
+ * @d: ...
+ *
+ * Return: ...
+ */
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->owner);
+free(d->name);
+free(d);
+}
+}
