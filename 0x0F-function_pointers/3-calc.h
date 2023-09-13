998012eb09a14158c2375508ac1e70b1c823cc47
Youssef <yusifmohamed2023@gmail.com>
2023-09-13 08:06:46 -0700

    hi

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,12 +1,11 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef CALC_H
+#define CALC_H
 
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
+*struct op - Struct op
+*@op: The operator
+*@f: The function associated
+*/
 typedef struct op
 {
 char *op;
@@ -19,4 +18,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
 #endif
