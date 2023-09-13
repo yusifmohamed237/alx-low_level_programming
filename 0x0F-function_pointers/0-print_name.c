5fb0bbde7bb3fbebab659242db72d38ab9491ff2
Youssef <yusifmohamed2023@gmail.com>
2023-09-13 08:10:08 -0700

    hi

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,18 @@
-#include "main.h"
-#include<stdlib.h>
+#include "function_pointers.h"
+
 /**
- * print_name - prints a name
- * @name: person's name
- * @f: pointer to a function
- * Return: void
- */
+*print_name - prints a name
+*@name:name to print
+*@f: function which print name
+*/
+
 void print_name(char *name, void (*f)(char *))
 {
-if (f != NULL)
+
+if (name == NULL || f == NULL)
+return;
+
 f(name);
 
 }
+
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,21 +1,23 @@
-#include "main.h"
+#include "function_pointers.h"
+
 /**
- * array_iterator - execute function on each element
- * of an array
- * @array: given array
- * @size: size of the array
- * @action: pointer to function
- * Return: void
- */
+*array_iterator - executes a function on each element of an array
+*@array: array to be targeted
+*@size: size of array
+*@action: function to be executed
+*
+*/
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-int i;
 
 if (array == NULL || action == NULL)
 return;
 
-for (i = 0; i < (int) size; i++)
+while (size-- > 0)
 {
-action(array[i]);
+action(*array);
+array++;
 }
+
 }
